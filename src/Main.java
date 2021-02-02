d702dd669f2c40af4e85dff0890f274c359ec3fe
maxwid <max.widell@hm.com>
2021-02-02 14:55:10 +0100

    Cleaned up code

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> movieList = List.of("Daredevil", "Matrix", "Gudfadern");
-        //movieList.stream().filter();
-        //String matrix =findByString(movieList, "Matrix");
-        //System.out.println(matrix);
-
-        String shortestMovie = findShortestString(movieList);
-        System.out.println(shortestMovie);
-    }
-
-    private static String findShortestString(List<String> movieList) {
-        String result = null;
-        String shortestMovie = "abrakadabrsdasdsadadadsadasdasdasdadaa";
-
-        for (String m : movieList) {
-            if (m.length() <= shortestMovie.length() ) {
-               result = m;
-            }
-        }
-        return result;
-    }
-
-    private static String findByString(List<String> movieList, String str) {
-        String result = null;
-        for (String movieName: movieList) {
-            if (movieName.equals(str)) {
-                result = str;
-                break;
-            }
-        }
-        return result;
+        System.out.println();
     }
 }
